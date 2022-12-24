866b1239c2d4e66965626b08c6d844cf0df5bd20
Petmel7 <115225282+Petmel7@users.noreply.github.com>
2022-12-24 19:51:43 +0200

    add

---
+++ js/hero.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    function checWidth() {
+        var windowWidth = $('body').innerWielem = $(".number-bullets"); //
+                                                                       //
+        if(windowWidth < 480){
+            elem.removeClass('container');
+            elem.addClass('container');
+
+        }
+        else{
+            elem.removeClass('container');
+            elem.addClass('container');
+        }
+    }
+});
