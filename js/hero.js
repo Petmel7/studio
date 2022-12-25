3bc1c6e062811f63502ea690d2cda509facaa4e0
Petmel7 <115225282+Petmel7@users.noreply.github.com>
2022-12-25 19:06:12 +0200

    add

--- js/hero.js
+++ js/hero.js
@@ -1,15 +1,22 @@
-$(document).ready(function() {
-    function checWidth() {
-        var windowWidth = $('body').innerWielem = $(".number-bullets"); //
-                                                                       //
-        if(windowWidth < 480){
-            elem.removeClass('container');
-            elem.addClass('container');
+// $(document).ready(function() {
+//     function checWidth() {
+//         var windowWidth = $('body').innerWielem = $(".number-bullets"); //
+//                                                                        //
+//         if(windowWidth < 480){
+//             elem.removeClass('container');
+//             elem.addClass('container');
 
-        }
-        else{
-            elem.removeClass('container');
-            elem.addClass('container');
-        }
-    }
-});
+//         }
+//         else{
+//             elem.removeClass('container');
+//             elem.addClass('container');
+//         }
+//     }
+// });
+
+// (() => {
+//     const menuBtnRef = document.querySelector("[data-hero]");
+//     menuBtnRef.addEventListener("windowWidth < 480", () => {
+//         menuBtnRef.classList.toggle("container"); 
+//     });
+// })();
--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,14 @@
 (() => {
     const menuBtnRef = document.querySelector("[data-menu-button]");
+    const mobileMenuRef = document.querySelector("[data-menu");
+
     menuBtnRef.addEventListener("click", () => {
-        menuBtnRef.classList.toggle("is-open"); 
+        const expanded =
+        menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+
+        menuBtnRef.classList.toggle("is-open");
+        menuBtnRef.setAttribute("aria-exspanded", !expanded);
+
+        mobileMenuRef.classList.toggle("is-open");
     });
 })();
