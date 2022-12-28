6b244493ef1c2e73cf81587dda2a08d549031476
Petmel7 <115225282+Petmel7@users.noreply.github.com>
2022-12-28 19:37:43 +0200

    add

--- js/hero.js
+++ js/hero.js
@@ -1,22 +1,27 @@
 // $(document).ready(function() {
 //     function checWidth() {
-//         // var windowWidth = $('body').innerWielem = $(".number-bullets"); //
-//         //                                                                //
-//         if(windowWidth < 480){
+//         var windowWidth = $('body').innerWielem = $(".number-bullets");
+
+//         if(windowWidth > 480){
 //             elem.removeClass('container');
-//             elem.addClass('container');
+//             // elem.addClass('container');
 
 //         }
 //         else{
 //             elem.removeClass('container');
-//             elem.addClass('container');
+//             // elem.addClass('container');
 //         }
 //     }
 // });
 
-// (() => {
-//     const menuBtnRef = document.querySelector("[data-hero]");
-//     menuBtnRef.addEventListener("windowWidth < 480", () => {
-//         menuBtnRef.classList.toggle("container"); 
-//     });
-// })();
+(() => {
+    const heroRef = document.querySelector("[data-hero]");
+        if(windowWidth > 480){
+            elem.removeClass('container');
+
+        }
+        else{
+            elem.removeClass('container');
+        }
+    }
+);
