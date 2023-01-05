a27b8fb7ce1537e9ae4fdcc3b4e03ba64072e9a6
Petmel7 <115225282+Petmel7@users.noreply.github.com>
2023-01-05 17:25:00 +0200

    add

---
+++ lesson/lesson.js
@@ -0,0 +1,71 @@
+// if else and prompt
+
+// 1) Попросити юзера число
+// 2) Попросити чи рівне воно 12
+// 3) На основі перевірки запустити if або else
+
+// const userNumber = Number(prompt('Enter number'));
+
+// if (userNumber === 12) {
+//     console.log('Correct');
+// } else {
+//     console.log('Error');
+// }
+
+// Програма яка получае від юзера число і оприділить парне воно чи не парне
+
+// 1) Получити число
+// 2) Написати перевірку на парне не парне число
+// 3) Вивести результат
+
+// const day = prompt('Enter day');
+
+// if (day % 2 === 0); {
+//     console.log('Парне');
+// } else {
+//     console.log('Непарне');
+// }
+
+// Програма: питаєм в юзера номер місяця і показуєм до якої пори року він відноситься
+
+// 1) Получити дані від юзера
+// 2) Перевірка на пору року
+// 3) ивести результат
+
+// Перший варіант
+
+// const monthNumber = prompt('Enter month number');
+
+// if (monthNumber === 1) {
+//     console.log('Зима');
+// } else if (monthNumber === 2) {
+//     console.log('Зима');
+// } else if (monthNumber === 12) {
+//     console.log('Зима');
+// }
+
+// Другий варіант, коротший
+
+// const monthNumber = prompt('Enter month number');
+
+// if (monthNumber === 1 || monthNumber == 2 || monthNumber === 12) {
+//     console.log('Зима');
+// } else if (monthNumber === 3 || monthNumber == 4 || monthNumber === 5) {
+//     console.log('Весна');
+// } else if (monthNumber === 6 || monthNumber === 7 || monthNumber === 8) {
+//     console.log('Літо');
+// } else if (monthNumber === 9 || monthNumber === 10 || monthNumber === 11) {
+//     console.log('Осінь');
+// }
+
+// Третій варіант
+
+// const monthNumber = prompt('Enter month number');
+
+// if (monthNumber => 3 && monthNumber <= 5) {
+//     console.log('Весна')
+// } else if (monthNumber => 6 && monthNumber <= 8) {
+//     console.log('Літо');
+// } else if (monthNumber => 9 && monthNumber <= 11) {
+//     console.log('Осінь');
+// } 
