a34bd474cae878793b226850e33a3c49194c46cb
Petmel7 <115225282+Petmel7@users.noreply.github.com>
2023-04-01 13:43:16 +0300

    add

--- task/task.js
+++ task/task.js
@@ -1,1 +1,12 @@
-const categoriesReff = document.querySelector('#categories');
+const categoriesList = document.querySelector('#categories');
+const categoriesItems = document.querySelectorAll('.item');
+
+console.log(`Категорії ${categoriesItems.length}`);
+
+categoriesItems.forEach (item => {
+    const categoriesName = item.querySelector('h2');
+    const categoriesItemsCaunt = item.querySelectorAll('li');
+
+    console.log(categoriesName.textContent);
+    console.log(`Кількість ${categoriesItemsCaunt.length}`);
+});
