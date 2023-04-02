044921be9ef74aed055afe282177bbe65ef161fb
Petmel7 <115225282+Petmel7@users.noreply.github.com>
2023-04-02 16:15:18 +0300

    add

--- js/modal.js
+++ js/modal.js
@@ -13,10 +13,14 @@
     } 
   })();
 
-// const modal = document.querySelector('.backdrop');
+
+
+
+
+// const dataModal = document.querySelector('.backdrop');
 // const modalForm = document.querySelector('.js-speacer-form');
 
-// Отримати елементи форми
+// // Отримати елементи форми
 // const nameInput = document.getElementById('name');
 // const telInput = document.getElementById('tel');
 // const mailInput = document.getElementById('mail');
@@ -59,13 +63,13 @@
 // }
 
 // // Додати обробники подій на кнопки
-// // modal.addEventListener('click', function(event) {
-// //   if (event.target.classList.contains('modal-cross__button') || event.target.classList.contains('backdrop')) {
-// //     hideModal();
-// //   }
-// // });
+// modal.addEventListener('click', function(event) {
+//   if (event.target.classList.contains('modal-cross__button') || event.target.classList.contains('backdrop')) {
+//     hideModal();
+//   }
+// });
 
-// // modalForm.addEventListener('submit', handleFormSubmit);
+// modalForm.addEventListener('submit', handleFormSubmit);
 
 // const modalWrapper = document.querySelector('.modal-wrapper');
 // const modal = modalWrapper.querySelector('.modal');
@@ -116,4 +120,107 @@
 
 
 
-  
+
+
+
+// const modalWrapper = document.querySelector('.modal-wrapper');
+// const modal = modalWrapper.querySelector('.modal');
+// const closeButton = modal.querySelector('.modal-close');
+// const form = modal.querySelector('.js-speaker-form');
+
+// // Показати модальне вікно
+// function openModal() {
+//   modalWrapper.classList.add('open');
+//   document.body.classList.add('no-scroll');
+// }
+
+// // Сховати модальне вікно
+// function closeModal() {
+//   modalWrapper.classList.remove('open');
+//   document.body.classList.remove('no-scroll');
+// }
+
+// // Закриття модального вікна при натисканні на кнопку закриття
+// closeButton.addEventListener('click', closeModal);
+
+// // Закриття модального вікна при натисканні на затемнену область вікна
+// modalWrapper.addEventListener('click', function(event) {
+//   if (event.target === modalWrapper) {
+//     closeModal();
+//   }
+// });
+
+// // Закриття модального вікна при натисканні клавіші "Escape"
+// document.addEventListener('keydown', function(event) {
+//   if (event.key === 'Escape') {
+//     closeModal();
+//   }
+// });
+
+// // Обробка події submit форми
+// form.addEventListener('submit', function(event) {
+//   event.preventDefault();
+//   // Код для відправки форми на сервер
+//   closeModal();
+// });
+
+// // Відкриття модального вікна при натисканні на кнопку або посилання
+// const openModalButtons = document.querySelectorAll('[data-modal]');
+// openModalButtons.forEach(function(button) {
+//   button.addEventListener('click', openModal);
+// });
+
+
+
+ 
+
+// Отримуємо модальне вікно
+// let modal = document.getElementById(".backdrop");
+let modal = document.querySelector(".backdrop");
+// console.log(modal);
+
+// Отримуємо кнопку, яка відкриває модальне вікно
+let btn = document.getElementsByClassName("hero-button")[0];
+// console.log(btn);
+
+// Отримуємо елемент span, який закриває модальне вікно
+let button = document.getElementsByClassName("modal-cross__button")[0];
+// console.log(button);
+
+// Отримуємо форму
+let form = document.querySelector('form js-speacer-form');
+// console.log(form);
+
+// Отримуємо елементи форми
+let nameInput = document.getElementById("name");
+let telInput = document.getElementById("tel");
+let mailInput = document.getElementById("mail")
+let textareaInput = document.getElementById("textarea");
+// console.log(nameInput);
+// console.log(telInput);
+// console.log(mailInput);
+// console.log(textareaInput);
+
+// Додаємо обробник події для кнопки відправити
+form.addEventListener('submit', function(e) {
+  e.preventDefault(); // запобігаємо перезавантаженню сторінки після натискання кнопки відправити
+  let name = nameInput.value;
+  let tel = telInput.value;
+  let mail = mailInput.value;
+  let textarea = textareaInput.value;
+  console.log("Ім'я: " + name);
+  console.log("Телефон: " + tel);
+  console.log("Пошта: " + mail);
+  console.log("Повідомлення: " + textarea);
+  modal.style.display = "none"; // закриваємо модальне вікно після відправлення форми
+});
+
+// Додаємо обробник події для кнопки відкриття модального вікна
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// Додаємо обробник події для закривання модального вікна
+button.onclick = function() {
+  modal.style.display = "none";
+}
--- task/task.js
+++ task/task.js
@@ -1,12 +1,12 @@
-const categoriesList = document.querySelector('#categories');
-const categoriesItems = document.querySelectorAll('.item');
+const categoriesId = document.querySelector('#categories');
+const categoriesItem = document.querySelectorAll('.item');
 
-console.log(`Категорії ${categoriesItems.length}`);
+console.log(`Кількість ${categoriesItem.length}`);
 
-categoriesItems.forEach (item => {
+categoriesItem.forEach(item => {
     const categoriesName = item.querySelector('h2');
-    const categoriesItemsCaunt = item.querySelectorAll('li');
+    const categoriesItemLi = item.querySelectorAll('li');
 
     console.log(categoriesName.textContent);
-    console.log(`Кількість ${categoriesItemsCaunt.length}`);
+    console.log(`Кількість ${categoriesItemLi.length}`);
 });
